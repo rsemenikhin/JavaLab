20ba82af4db0c8c2b837a2b56d20817747570d56
rsemenikhin <rom48r@gmail.com>
2026-04-04 19:03:06 +0300

    add application entry point

---
+++ src/main/java/ru/mpei/lab4/App.java
@@ -0,0 +1,20 @@
+package ru.mpei.lab4;
+
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.IOException;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws IOException, CsvValidationException {
+        CsvPersonReader reader = new CsvPersonReader();
+        List<Person> people = reader.readPeople();
+
+        System.out.println("Количество людей: " + people.size());
+        System.out.println("Первые 10 записей:");
+
+        for (int i = 0; i < people.size() && i < 10; i++) {
+            System.out.println(people.get(i));
+        }
+    }
+}
